5ec31a051a073336c4ff9884c5ce6ac132ec8b1c
i-tried-to-use-arch-btw <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 01:13:50 +0530

    undoing idk

--- hosts/home.nix
+++ hosts/home.nix
@@ -21,7 +21,6 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home-manager.backupFileExtension = "backup";
 
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
