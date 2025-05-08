471390ebef670c78428dae01608826c52d0d3a7b
i-tried-to-use-arch-btw <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 01:07:59 +0530

    Lets try adding hyprland config with dvorak

---
+++ hosts/HomeManager/hyprland.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ...}:
+
+{
+  wayland.windowmanagers.hyprland = {
+    # Allows HomeManger to manage Hyprland
+    enable = true;
+    xwayland.enable = true;
+
+    settings = {
+      input = {
+        kb_layout = "us";
+        kb_variant = "dvorak";
+      };
+    }
+  };
+}
