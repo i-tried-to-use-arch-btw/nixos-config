439611998ea01dba0a90ef59baad44ee2c158c80
i-tried-to-use-arch-btw <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-08 21:19:57 +0530

    Using flakes for Hyprland

--- hosts/hyprland.nix
+++ hosts/hyprland.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ...}:
+{ config, pkgs, inputs, ...}:
 
 {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
 
   environment.sessionVariables = {
