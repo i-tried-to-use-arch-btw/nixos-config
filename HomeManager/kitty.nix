b7d44b057801ba94b1a07fc507dedd98d07dddab
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 23:39:09 +0530

    Adding a kitty config

---
+++ HomeManager/kitty.nix
@@ -0,0 +1,22 @@
+{ pkg, lib, config, ... }:
+
+{
+  programs.kitty = {
+  enable = true;
+
+  font = {
+    name = "JetBrainsMono Nerd Font";
+    size = 12;
+  };
+
+  theme = "Gruvbox Dark";
+
+  settings = {
+    cursor_shape = "beam";
+    enable_audio_bell = false;
+    scrollback_lines = 5000;
+    confirm_os_window_close = 0;
+  };
+};
+
+}
