ba60cc13b87d0d6941d45c262a171285dc918b37
Your Name <your@email.com>
2025-05-09 12:44:40 +0530

    refactor: move hyprland configuration to a new directory and update import path

--- hosts/home.nix
+++ hosts/home.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     # hyprland
-    ./HomeManager/hyprland.nix
+    ./HomeManager/Hyprland/hyprland.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
