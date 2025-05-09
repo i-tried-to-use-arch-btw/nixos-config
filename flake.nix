b1cf1f7c9cc3035289c946d0caa3b6592fa66f1a
206698572+i-tried-to-use-arch-btw@users.noreply.github.com <your@email.com>
2025-05-09 14:36:50 +0530

    refactor: comment out home-manager configuration in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
@@ -18,7 +18,7 @@
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/configuration.nix
-        inputs.home-manager.nixosModules.default
+        # inputs.home-manager.nixosModules.default
       ];
     };
   };
