c5ad3f59fb0e08c5d1c9f4072c6528657bbb6ad1
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 18:51:31 +0530

    its just too sad

--- HomeManager/git.nix
+++
@@ -1,9 +0,0 @@
-   { pkgs, ... }:
-
-   {
-     programs.git = {
-       enable = true;
-       userName = "NixUser";
-       userEmail = "206698572+i-tried-to-use-arch-btw@users.noreply.github.com";
-     };
-   }
