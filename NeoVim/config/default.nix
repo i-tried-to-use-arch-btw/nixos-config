4aceb64a1827f6f6b9be9ec497c991a80b5867d7
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 21:02:35 +0530

    feat: uncomment bufferline module import in Neovim configuration

--- NeoVim/config/default.nix
+++ NeoVim/config/default.nix
@@ -1,4 +1,21 @@
 {
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+  # imports = [ ./bufferline.nix ];
+
+  colorschemes.gruvbox.enable = true;
+
+  plugins = {
+    lualine.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+        tsserver.enable = true;
+        pyright.enable = true;
+        gopls.enable = true;
+        rust_analyzer.enable = true;
+        eslint.enable = true;
+        lua_ls.enable = true;
+      };
+    };
+  };
 }
