e766d7e44e235a1b7fcf0d75acf5f3c9d2ae59bb
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 20:07:38 +0530

    feat: enable Zsh management in Home Manager with shell aliases and Oh My Zsh configuration

--- HomeManager/zsh.nix
+++ HomeManager/zsh.nix
@@ -1,4 +1,15 @@
 { config, lib, pkgs, ... }:
 {
-  '';
+  programs.zsh = {
+    # Allow Home manager to manage Zsh
+    enable = true;
+    shellAliases = {
+      update = "sudo nixos-rebuild switch";
+    };
+  };
+  oh-my-zsh = {
+    enable = true;
+    plugins = [ ];
+    theme = "agnoster";
+  };
 }
