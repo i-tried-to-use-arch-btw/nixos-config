f622f18a69d71805b8df9f7f1e75ec17f72a0bef
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 22:53:24 +0530

    feat: restructure Nix configuration for Neovim to improve readability

--- HomeManager/NeoVim/config/default.nix
+++ HomeManager/NeoVim/config/default.nix
@@ -1,20 +1,24 @@
+{ pkgs, config, lib, ... }:
+
 {
-  # Import all your configuration modules here
-  # imports = [ ./bufferline.nix ];
-
-  colorschemes.gruvbox.enable = true;
-
-  plugins = {
-    lualine.enable = true;
-    lsp = {
-      enable = true;
-      servers = {
-        tsserver.enable = true;
-        pyright.enable = true;
-        gopls.enable = true;
-        rust_analyzer.enable = true;
-        eslint.enable = true;
-        lua_ls.enable = true;
+  programs.nixvim = {
+    # Import all your configuration modules here
+    # imports = [ ./bufferline.nix ];
+  
+    colorschemes.gruvbox.enable = true;
+  
+    plugins = {
+      lualine.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          tsserver.enable = true;
+          pyright.enable = true;
+          gopls.enable = true;
+          rust_analyzer.enable = true;
+          eslint.enable = true;
+          lua_ls.enable = true;
+        };
       };
     };
   };
