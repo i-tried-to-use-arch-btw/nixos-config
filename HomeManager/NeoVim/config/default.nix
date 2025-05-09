6bb8f88faaa81f9fc287193065e9076550c16ae8
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 23:11:41 +0530

    feat: update Nix configuration for Nixvim to fix server names and add extra Lua configuration

--- HomeManager/NeoVim/config/default.nix
+++ HomeManager/NeoVim/config/default.nix
@@ -4,22 +4,32 @@
   programs.nixvim = {
     # Import all your configuration modules here
     # imports = [ ./bufferline.nix ];
-  
+
     colorschemes.gruvbox.enable = true;
-  
+
     plugins = {
       lualine.enable = true;
       lsp = {
         enable = true;
         servers = {
-          tsserver.enable = true;
+          ts_ls.enable = true;
           pyright.enable = true;
           gopls.enable = true;
-          rust_analyzer.enable = true;
           eslint.enable = true;
           lua_ls.enable = true;
         };
       };
     };
+
+    extraConfigLua = ''
+      -- Clean UI
+      vim.opt.fillchars:append { eob = " " }   -- No ~ on empty lines
+      vim.opt.shortmess:append "I"             -- Disable startup message
+
+      -- Tab size settings
+      vim.opt.tabstop = 2         -- Number of spaces a tab counts for
+      vim.opt.shiftwidth = 2      -- Number of spaces for each indentation
+      vim.opt.expandtab = true    -- Convert tabs to spaces
+    '';
   };
 }
