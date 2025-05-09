873f8353e87979353fb3015f391906281e405266
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-09 15:36:38 +0530

    feat: add Hyprland and Git configurations to Home Manager

---
+++ HomeManager/Hyprland/hyprland.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland. = {
+    # Allow HomeManager to manage Hyprland
+    enable = true;
+    settings = {
+      "$mainMod" = "Super";
+      
+    };
+  }; 
+}
