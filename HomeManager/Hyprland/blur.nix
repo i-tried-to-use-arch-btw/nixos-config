532ad6b4a3ae9eff69a454e9f1d1333a819e99ce
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-10 14:36:28 +0530

    un enabling blur

--- HomeManager/Hyprland/blur.nix
+++ HomeManager/Hyprland/blur.nix
@@ -3,12 +3,6 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
-      decoration = {
-        blur = yes;
-        blur_size = 10;
-        blur_passes = 2;
-        blur_new_optimizations = yes;
-      };
     };
   };
 }
