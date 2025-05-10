f806775c95cf242bb8e0ab094cae480c2d428425
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-10 13:31:34 +0530

    did another wallpaper

--- HomeManager/Hyprland/walls.nix
+++ HomeManager/Hyprland/walls.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       exec-once = [
-        "swaybg -i /home/me/nixos-config/Walls/gruvbox-nix.jpg -m fill"
+        "swaybg -i /home/me/nixos-config/Walls/space.png -m fill"
         "hyprctl setcursor Adwaita 24"
       ];
     };
