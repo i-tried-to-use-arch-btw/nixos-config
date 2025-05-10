ff520b46e5ed0e3a1ef45fe9d0553031dd801ed2
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-10 21:10:01 +0530

    add blur inactive-transparnecy changes

--- HomeManager/Hyprland/decoration.nix
+++ HomeManager/Hyprland/decoration.nix
@@ -5,13 +5,14 @@
     settings = {
       decoration = {
         rounding = 10;
+        inactive_opacity = 0.8;
 
         blur = {
           enabled = true;
           xray = true;
           special = false;
           new_optimizations = true;
-          size = 14;
+          size = 8;
           passes = 4;
           brightness = 1;
           noise = 0.01;
