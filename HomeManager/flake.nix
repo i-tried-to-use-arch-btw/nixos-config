29434380fd1fc414cea50035d85ea780a682cfd9
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-16 23:06:40 +0530

    instaled astal with ags but didn't get it working

--- HomeManager/flake.nix
+++ HomeManager/flake.nix
@@ -13,10 +13,19 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }: 
+  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, astal, ags, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -27,6 +36,28 @@
         extraSpecialArgs = {
           inherit nixvim inputs;
         };
+      };
+
+      # Move the `packages.${system}.default` definition inside the `outputs` block
+      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation rec {
+        name = "my-shell";
+        src = ./.;
+
+        nativeBuildInputs = [
+          ags.packages.${system}.default
+          pkgs.wrapGAppsHook
+          pkgs.gobject-introspection
+        ];
+
+        buildInputs = with astal.packages.${system}; [
+          astal3
+          io
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          ags bundle app.ts $out/bin/${name}
+        '';
       };
     };
 }
