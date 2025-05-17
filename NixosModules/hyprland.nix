8b71c70c384afa20b7b33180a84806a0ff01a7a9
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-17 12:06:00 +0530

    remove useless hyprland options

--- NixosModules/hyprland.nix
+++ NixosModules/hyprland.nix
@@ -10,15 +10,13 @@
   environment.sessionVariables = {
     # If your cursor becomes invisible
     WLR_NO_HARDWARE_CURSORS = "1";
-    # Hint electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
   };
 
   hardware = {
     # Opengl
-    opengl.enable = true;
+    #opengl.enable = true;
     # Most wayland compositors need this
-    nvidia.modesetting.enable = true;
+    #nvidia.modesetting.enable = true;
   };
   environment.systemPackages = with pkgs; [
     # Hyprland
