f36488e751e837c164a3ea7154ee41a9ed8a623b
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-20 15:17:05 +0530

    kitty transparency gone

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -14,7 +14,7 @@
       enable_audio_bell = false;
       scrollback_lines = 5000;
       confirm_os_window_close = 0;
-      background_opacity = 0.8;
+      background_opacity = 1;
       window_padding_width = 12;
     };
 
