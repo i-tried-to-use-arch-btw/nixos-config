955192b78ad4d02413fb633bada871c3be1739cb
NixUser <206698572+i-tried-to-use-arch-btw@users.noreply.github.com>
2025-05-20 14:42:51 +0530

    remove kitty padding

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -17,7 +17,7 @@
       scrollback_lines = 5000;
       confirm_os_window_close = 0;
       background_opacity = 0.8;
-      window_padding_width = 12;
+      # window_padding_width = 12;
     };
   };
 }
