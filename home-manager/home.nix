9655dafda3269b0ce94f92e03970204a316539ad
nurlyx <206698572+nurlyx@users.noreply.github.com>
2025-05-23 22:34:19 +0530

    change gtk theme to dark

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,6 +19,12 @@ in
     ./symlinks.nix
   ];
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
